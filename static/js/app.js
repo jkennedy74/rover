4f86fb45b3d72beab7200c97dde32ee68744e8ae
Juliana Kennedy <jkennedy@firstlook.biz>
2018-07-27 16:33:35 -0500

    fina

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,108 @@
+//Make a copy!
+var dataset = data
+
+var $tbody = document.querySelector("tbody");
+var $dateInput = document.querySelector("#date");
+var $cityInput = document.querySelector("#city");
+var $stateInput = document.querySelector("#state");
+var $countryInput = document.querySelector("#country");
+var $shapeInput = document.querySelector("#shape");
+
+// Get Buttons
+var $searchBtn = document.querySelector("#search");
+var $resetBtn = document.querySelector("#reset");
+var $resetBtnMain = document.querySelector("#reset-main");
 
 
-$(document).ready(function() {
-    // Setup - add a text input to each footer cell
-    $('#example tfoot th').each( function () {
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-    } );
+// Event listeners on modal search
+$searchBtn.addEventListener("click", handleSearchButtonClick);
+$resetBtn.addEventListener("click", handleResetButtonClick);
+$resetBtnMain.addEventListener("click", handleResetButtonClick);
+
+
+var filteredData = dataset;
+
+// Dynamic Table definition
+function renderTable() {
+  $tbody.innerHTML = "";
+  for (var i = 0; i < filteredData.length; i++) {
+    var sighting = filteredData[i];
+    var fields = Object.keys(sighting);
+    var $row = $tbody.insertRow(i);
+    for (var j = 0; j < fields.length; j++) {
+      var field = fields[j];
+      var $cell = $row.insertCell(j);
+      $cell.innerText = sighting[field];
+    }
+  }
+}
+
+function handleSearchButtonClick() {
+
+  var filterDate = $dateInput.value.trim();
+  if (filterDate != "") {
+    filteredData = dataSet.filter(function (sighting) {
+      var sightingDate = sighting.datetime;
+      return sightingDate === filterDate;
+    });
+  };
+  var filterCity = $cityInput.value.trim().toLowerCase();
+  if (filterCity != "") {
+    filteredData = filteredData.filter(function (sighting) {
+      var sightingCity = sighting.city;
+      return sightingCity === filterCity;
+    });
+  };
+  var filterState = $stateInput.value.trim().toLowerCase();
+  if (filterState != "") {
+    filteredData = filteredData.filter(function (sighting) {
+      var sightingState = sighting.state;
+      return sightingState === filterState;
+    });
+  };
+  var filterCountry = $countryInput.value.trim().toLowerCase();
+  if (filterCountry != "") {
+    filteredData = filteredData.filter(function (sighting) {
+      var sightingCountry = sighting.country;
+      return sightingCountry === filterCountry;
+    });
+  };
+  var filterShape = $shapeInput.value.trim().toLowerCase();
+  if (filterShape != "") {
+    filteredData = filteredData.filter(function (sighting) {
+      var sightingShape = sighting.shape;
+      return sightingShape === filterShape;
+    });
+  };
+  renderTable();
+};
+
+
+// Reset the data and search form after a search
+function handleResetButtonClick() {
+  filteredData = data;
+  $dateInput.value = "";
+  $cityInput.value = "";
+  $stateInput.value = "";
+  $countryInput.value = "";
+  $shapeInput.value = "";
+  renderTable();
+}
+
+// Render the table for the first time on page load
+renderTable();
+
+
+console.log(data);
+
+var comments = data.map(data => data.comments);
   
-    // from data.js;
-    var table = data;
-  
-    // Apply the search
-    table.columns().every( function () {
-        var that = this;
-  
-        $( 'input', this.footer() ).on( 'keyup change', function () {
-            if ( that.search() !== this.value ) {
-                that
-                    .search( this.value )
-                    .draw();
-            }
-        } );
-    } );
-} );
 
+//Just for fun, lets highlight some random comments.
+var rand = Math.floor(Math.random() * comments.length);
 
-///d3.select("tbody").html("")
+function showquote(){
+    document.getElementById('quote').innerHTML = comments[rand];
+    console.log(comments[rand])
+}
+showquote();
