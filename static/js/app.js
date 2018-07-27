9585671a403d3bc7bea8111486e4cb9ea98bab8e
jkennedy74 <33709673+jkennedy74@users.noreply.github.com>
2018-07-27 09:57:03 -0500

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,28 @@
-// from data.js;
-var tableData = data;
 
 
-// YOUR CODE HERE!
+$(document).ready(function() {
+    // Setup - add a text input to each footer cell
+    $('#example tfoot th').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    } );
+  
+    // from data.js;
+    var table = data;
+  
+    // Apply the search
+    table.columns().every( function () {
+        var that = this;
+  
+        $( 'input', this.footer() ).on( 'keyup change', function () {
+            if ( that.search() !== this.value ) {
+                that
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );
+} );
 
-console.log(tableData);
+
+///d3.select("tbody").html("")
